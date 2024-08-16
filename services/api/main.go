2c9ee143cb969b96d780510f04888be22a0d85c3
gabrielluizep <gabrielluizep@hotmail.com>
2024-08-16 16:54:45 -0300

    feat: added `127.0.0.1` to cors

--- services/api/main.go
+++ services/api/main.go
@@ -97,7 +97,7 @@ func main() {
 
 	router := gin.Default()
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowOrigins:     []string{"http://127.0.0.1:5173", "http://localhost:5173"},
 		AllowMethods:     []string{"GET", "POST"},
 		AllowHeaders:     []string{"Origin"},
 		ExposeHeaders:    []string{"Content-Length"},
