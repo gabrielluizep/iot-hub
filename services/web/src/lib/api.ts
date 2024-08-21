41916777be6aa9c337bb7e4574e033ea4236653f
gabrielluizep <gabrielluizep@hotmail.com>
2024-08-21 12:17:47 -0300

    feat: moved `http://` to env variable

--- services/web/src/lib/api.ts
+++ services/web/src/lib/api.ts
@@ -1,7 +1,7 @@
 const API_URL = import.meta.env.VITE_API_URL;
 
 export const getSensors = async () => {
-	const response = await fetch(`http://${API_URL}/sensors`);
+	const response = await fetch(`${API_URL}/sensors`);
 
 	console.log(API_URL);
 
@@ -10,7 +10,7 @@ export const getSensors = async () => {
 
 export const getSensorData = async (sensorId: number) => {
 	const response = await fetch(
-		`http://${API_URL}/sensors/${sensorId}/readings`,
+		`${API_URL}/sensors/${sensorId}/readings`,
 	);
 
 	return response.json();
