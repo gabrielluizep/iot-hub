d53165ebb542281981a0bf9e3c5e9e9361d2f7c1
gabrielluizep <gabrielluizep@hotmail.com>
2024-08-23 10:49:17 -0300

    feat: added send turn off light button

--- services/web/src/lib/api.ts
+++ services/web/src/lib/api.ts
@@ -3,15 +3,23 @@ const API_URL = import.meta.env.VITE_API_URL;
 export const getSensors = async () => {
 	const response = await fetch(`${API_URL}/sensors`);
 
-	console.log(API_URL);
-
 	return response.json();
 };
 
 export const getSensorData = async (sensorId: number) => {
-	const response = await fetch(
-		`${API_URL}/sensors/${sensorId}/readings`,
-	);
+	const response = await fetch(`${API_URL}/sensors/${sensorId}/readings`);
+
+	return response.json();
+};
+
+export const postSensorLight = async (sensorId: number, lightOn: boolean) => {
+	const response = await fetch(`${API_URL}/sensors/${sensorId}`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({ lightOn }),
+	});
 
 	return response.json();
 };
--- services/web/src/pages/login.tsx
+++ services/web/src/pages/login.tsx
@@ -1,5 +1,5 @@
 import { Button } from '@/components/ui/button';
-import { getSensorData, getSensors } from '@/lib/api';
+import { getSensorData, getSensors, postSensorLight } from '@/lib/api';
 import { useQuery } from '@tanstack/react-query';
 import { useState } from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, Legend } from 'recharts';
@@ -16,12 +16,20 @@ export function MainPage() {
     queryKey: ['sensor-data', selectedSensor],
     // biome-ignore lint/style/noNonNullAssertion: <explanation>
     queryFn: () => getSensorData(selectedSensor!),
+    
     enabled: !!selectedSensor,
   });
 
+  // mutate to send a POST request to the server
+  const toggleLight = () => {
+    postSensorLight(selectedSensor!, !sensorData.at(-1).lightOn);
+  };
+
+  const lastReading = sensorData?.length > 0 ? sensorData.at(-1) : null;
+
   return (
     <div className="flex h-dvh">
-      <div className="border-2 w-48 flex justify-center py-16">
+      <div className="border-2 w-48 flex flex-col items-center py-16">
         {sensors?.map((sensor: number) => (
           <Button type="button" key={sensor} onClick={() => setSelectedSensor(sensor)}>
             Sensor {sensor}
@@ -29,16 +37,33 @@ export function MainPage() {
         ))}
       </div>
 
-      <div className="flex items-center justify-center flex-grow p-4">
+      <div className="flex flex-col items-center justify-center flex-grow p-4">
+        
         {sensorData ? (
+          <>
+          <div className='flex w-full '>
+          <h2 className='mr-auto font-bold text-2xl'>Sensor {selectedSensor}</h2>
+
+          <div className='flex items-center gap-2'>
+            {lastReading && (
+            <>
+                <p>Estado da luz: {lastReading.lightOn ? "ligada" : "desligada"}</p>
+              <Button size='sm' onClick={toggleLight}>Alterar estado</Button>
+            </>
+            )}
+          </div>
+        </div>
           <ResponsiveContainer width="100%" height="100%">
             <LineChart data={sensorData}>
               <CartesianGrid strokeDasharray="3 3" />
               <XAxis
                 dataKey="timestamp"
                 tickFormatter={(tick) => new Date(tick * 1000).toLocaleTimeString()} // Convert Unix timestamp to human-readable time
+                domain = {['auto', 'auto']}
+                type="number"
               />
-              <YAxis />
+              <YAxis 
+              />
               <Tooltip labelFormatter={(label) => new Date(label * 1000).toLocaleTimeString()} />
               <Legend />
               <Line type="monotone" dataKey="temperature" stroke="#ff7300" />
@@ -46,6 +71,7 @@ export function MainPage() {
               <Line type="monotone" dataKey="luminosity" stroke="#8884d8" />
             </LineChart>
           </ResponsiveContainer>
+          </>
         ) : (
           <p>Select a sensor to view data</p>
         )}
